a69aeeba66af2533f66d7df3a263789c1e998ba0
Yonatan Kra <kra.yonatan@gmail.com>
2026-09-02 07:25:55 +0300

    Support GitHub Pages preview base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://yonatankra.com',
+  base: process.env.PUBLIC_BASE_PATH || '/',
   trailingSlash: 'always',
   integrations: [mdx(), sitemap()],
 });
